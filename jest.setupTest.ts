6c0e29ce67b7f000fd1140ca90d564341dd604de
Juansasa <quavun@gmail.com>
2021-01-26 18:15:44 +0100

    Remove Jest

--- jest.setupTest.ts
+++
@@ -1,3 +0,0 @@
-import "jest-styled-components";
-import "@testing-library/jest-dom/extend-expect";
-import "snapshot-diff/extend-expect";
--- src/app/util.ts
+++
@@ -1,5 +0,0 @@
-export const OpenURLInWindow = (url: string) => (e: React.MouseEvent) => {
-  e.stopPropagation();
-  e.preventDefault();
-  window.open(url, "_blank");
-};
